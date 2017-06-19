be7b08d20f830b7828c1ce58d3747ff8a0b60579
HelenaAnufrieva <elen.nuf@gmail.com>
2017-06-19 19:24:46 +0300

    Update FireInTheShop.java

--- src/java15/entity/errors/FireInTheShop.java
+++ src/java15/entity/errors/FireInTheShop.java
@@ -1,7 +1,7 @@
 package java15.entity.errors;
 
-public class FireInTheShop extends Error {
-    private String message;
+public class FireInTheShop extends Error { // Runtime
+    private String message; // = "Fire"
     public FireInTheShop(String message)
     {
         this.message = message;
